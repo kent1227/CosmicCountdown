e500189bddd3254414ffffdbd112a5aebdd2bc99
kent1227 <kent1227@icloud.com>
2023-07-22 13:08:56 -0400

    planet

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+
+    public string name;
+    public string description;
+    public string sceneName;
+
+    public Component[] components;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
